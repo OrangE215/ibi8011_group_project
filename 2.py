ea2fcdd718a936a2e9970cf8f6bd27e859f11031
liyicheng215 <Yicheng3.22@intl.zju.edu.cn>
2023-05-07 10:01:37 +0100

    change

--- 2.py
+++ 2.py
@@ -1,37 +1,25 @@
 import re
-from itertools import product
 
-# Import IUPAC ambiguity codes for nucleotide degeneracy
-R = ['A', 'G']
-K = ['G', 'T']
-S = ['G', 'C']
-Y = ['C', 'T']
-M = ['A', 'C']
-W = ['A', 'T']
-B = ['C', 'G', 'T']
-H = ['A', 'C', 'T']
-N = ['A', 'C', 'G', 'T']
-D = ['A', 'G', 'T']
-V = ['A', 'C', 'G']
 
-# There is no spacer directly for nucleotides in each resulting sequence
-separator = ''
+def check_binding_with_ambiguities(DNA):
+    FOXC1_ambiguities = '[T|A][A|G][T|A][G|A][T|A|C][A|C]AA[T|C]A[T|A]'
+    FOXL1_ambiguities = '[G|A][T|C][A|C]AAA[C|T]A'
 
-print('The potential binding motifs for FOXC1 are:')
+    if set(DNA).issubset('ACTG'):
+        if re.search(FOXC1_ambiguities, DNA) and re.search(FOXL1_ambiguities, DNA):
+            result = 'The ambiguities in both FOXC1 and FOXL1 allow the binding'
+        elif re.search(FOXC1_ambiguities, DNA):
+            result = 'The ambiguities in FOXC1 allow the binding'
+        elif re.search(FOXL1_ambiguities, DNA):
+            result = 'The ambiguities in FOXL1 allow the binding'
+        else:
+            result = 'The ambiguities in FOXC1 and FOXL1 don not allow the binding'
+    else:
+        result = 'Warning! The input format is incorrect!'
 
-""" 
-use the fuction 'product' to caculate the Cartesian product to get possible combinations of the binding motifs for 
-FOXC1 and FOXL1and use the fuction 'join' to convert each result to a string 
-"""
-for FOXC1_combinations in product(W, R, W, R, H, M, ['A'], ['A'], Y, ['A'], W):
-    FOXC1_binding_motif = separator.join(FOXC1_combinations)
-    print(FOXC1_binding_motif, end=' ')
+    return result
 
-# Start on another line
-print('\n')
 
-print('The potential binding motifs for FOXL1 are:')
-
-for FOXL1_combinations in product(R, Y, M, ['A'], ['A'], ['A'], Y, ['A']):
-    FOXL1_binding_motif = separator.join(FOXL1_combinations)
-    print(FOXL1_binding_motif, end=' ')
+dna_sequence = 'ACAAAACA'
+a = check_binding_with_ambiguities(dna_sequence)
+print(a)
