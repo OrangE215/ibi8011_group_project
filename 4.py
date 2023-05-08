1b9be161a87776edb582114a52ac7aef5119bb73
Congtai_Zhou <127284132+abababababaa@users.noreply.github.com>
2023-05-08 20:16:12 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,39 +1,30 @@
 # task 4
-'''
-Can two transcription factors bind at the same location in this DNA?
-nonoverlapping
-non ambiguise
-'''
 import re
-seq=input('Please input the DNA sequence:')
-seq=seq.upper()
-FOXC1 = 'TATGTAAATAT' 
+seq = input('input the DNA sequence:')
+seq = seq.upper()
+FOXC1 = 'TATGTAAATAT'
 FOXL1 = 'GTAAACA'
-
-class motifinseq(object):
-   def __init__(self):
-      self.len=len(self)
-   def bindsites(self,tseq):
-     position = []
-     index=-1
-     while True:
-        index = tseq.find(self,index+1) #' ' is certain motif (what this mean?)
-        if index==-1:
-            break
-        position.append(index)
-     return position
-
-if re.search(r'[^ATCG]',seq):
-    print ('input is incorrect')
-else: 
-    print ('input is correct')
-    FOXC1p=motifinseq.bindsites(FOXC1,seq)
-    #print(FOXC1p)
-    FOXL1p=motifinseq.bindsites(FOXL1,seq)  # print(FOXL1p) / print(type(FOXC1p[1])) type: int
-    
-    if FOXC1p[0]+len(FOXC1) <= FOXL1p[-1]:
-       print("FOXC1 and FOXL1 CAN bind simultaneously.")
-    elif FOXL1p[0]+len(FOXL1) <= FOXC1p[-1]:
-       print("FOXC1 and FOXL1 CAN bind simultaneously.")
-    else:
-       print("FOXC1 and FOXL1 CANNOT bind simultaneously")
+class find(object):
+    def __init__(self):
+        self.len = len(self)
+    def position(self, tseq):
+        location = []
+        index = -1
+        while True:
+            index = tseq.find(self, index + 1)  # 如果匹配到一样的碱基，就继续匹配下一个，直到匹配结束或者失败
+            if index == -1:
+                break
+            location.append(index)
+        return location  # 返回值如果是-1，代表匹配不成功
+if re.search(r'[^ATCG]', seq):
+    print('The input is incorrect')
+else:
+    print('The input is correct')
+    FOXC1_p = find.position(FOXC1, seq)  # print(FOXC1p)
+    FOXL1_p = find.position(FOXL1, seq)  # print(FOXL1p)  print(type(FOXC1p[1])) type: int
+    for i in FOXC1_p:
+        for l in FOXL1_p:
+            if i + len(FOXC1) - l < 0 or i + len(FOXC1) - l >= len(FOXC1) + len(FOXL1):
+                print('can bind simultaneously')
+            else:
+                print('can not bind simultaneously')
