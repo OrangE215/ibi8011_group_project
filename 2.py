b7b308bf31db42b3a1f73798e086a06f48504d99
guobiao-ye <127282285+guobiao-ye@users.noreply.github.com>
2023-05-11 21:46:50 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,10 +2,14 @@ import re
 
 
 def check_binding_with_ambiguities(DNA):
+    # Use regular expressions to express the ambiguities in FOXC1 or FOXL1
     FOXC1_ambiguities = '[T|A][A|G][T|A][G|A][T|A|C][A|C]AA[T|C]A[T|A]'
     FOXL1_ambiguities = '[G|A][T|C][A|C]AA[C|T]A'
-
+    
+    # Check whether the input format is correct, if not, give a warning
     if set(DNA).issubset('ACTG'):
+        
+        # Check if the ambiguities in FOXC1 or FOXL1 allow them to bind this DNA
         if re.search(FOXC1_ambiguities, DNA) and re.search(FOXL1_ambiguities, DNA):
             print('The ambiguities in both FOXC1 and FOXL1 allow the binding')
         elif re.search(FOXC1_ambiguities, DNA):
