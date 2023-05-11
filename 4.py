818e6a8310332b676bf3cda8c1b705735e10f04d
liyicheng215 <127249671+liyicheng215@users.noreply.github.com>
2023-05-11 20:19:52 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -7,7 +7,7 @@ FOXL1 = 'GTAAACA'
 def check_both_binding(DNA):
     FOXC1_position = [match.start() for match in re.finditer(FOXC1, DNA)]
     FOXL1_position = [match.start() for match in re.finditer(FOXL1, DNA)]
-    if FOXC1_position == [] or FOXL1_position == []:  # Check if hve both motif
+    if FOXC1_position == [] or FOXL1_position == []:  # Check if have both motif
         return 'can not bind simultaneously'
     else:  # Check if there are at least two position with no overlap
         for i in FOXC1_position:
